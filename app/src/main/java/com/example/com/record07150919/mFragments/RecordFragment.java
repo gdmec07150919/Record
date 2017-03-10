af5f8806b0b5f386599a9add1ea55ff837abb804
GDMEC07150919 <1044934446@qq.com>
2017-03-10 09:53:13 +0800

    dsaf

--- app/src/main/java/com/example/com/record07150919/mFragments/RecordFragment.java
+++ app/src/main/java/com/example/com/record07150919/mFragments/RecordFragment.java
@@ -1,9 +1,12 @@
 package com.example.com.record07150919.mFragments;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.LayoutInflater;
@@ -15,23 +18,28 @@ import android.widget.CompoundButton;
 import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.EditText;
 
+import com.example.com.record07150919.DatePickerFragment;
 import com.example.com.record07150919.R;
 import com.example.com.record07150919.Record;
 import com.example.com.record07150919.RecordActivity;
 import com.example.com.record07150919.RecordLab;
 
+import java.util.Date;
 import java.util.UUID;
 
 public class RecordFragment extends Fragment {
     private static final String ARG_RECORD_ID = "record_id";
+    private static final String DIALOG_DATE = "DialogDate";
+    private static final int REQUEST_DATE = 0;
 
     private Record mRecord;
     private EditText mTitleField;
     private Button mDateButton;
     private CheckBox mSolvedCheckBox;
-    public static RecordFragment newInstance(UUID recordId){
+
+    public static RecordFragment newInstance(UUID recordId) {
         Bundle args = new Bundle();
-        args.putSerializable(ARG_RECORD_ID,recordId);
+        args.putSerializable(ARG_RECORD_ID, recordId);
         RecordFragment fragment = new RecordFragment();
         fragment.setArguments(args);
         return fragment;
@@ -40,10 +48,11 @@ public class RecordFragment extends Fragment {
     public RecordFragment() {
 
     }
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-       // UUID recordId = (UUID)getActivity().getIntent().getSerializableExtra(RecordActivity.EXTRA_RECORD_ID);
+        // UUID recordId = (UUID)getActivity().getIntent().getSerializableExtra(RecordActivity.EXTRA_RECORD_ID);
         UUID recordId = (UUID) getArguments().getSerializable(ARG_RECORD_ID);
         mRecord = RecordLab.get(getActivity()).getRecord(recordId);
     }
@@ -51,7 +60,20 @@ public class RecordFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_record,container,false);
+        View v = inflater.inflate(R.layout.fragment_record, container, false);
+
+        mDateButton = (Button) v.findViewById(R.id.record_date);
+        mDateButton.setText(mRecord.getmDate().toString());
+        mDateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fragmentManager = getFragmentManager();
+                DatePickerFragment datePickerFragment = DatePickerFragment.newInstance(mRecord.getmDate());
+                datePickerFragment.setTargetFragment(RecordFragment.this, REQUEST_DATE);
+                datePickerFragment.show(fragmentManager, DIALOG_DATE);
+            }
+        });
+
         mTitleField = (EditText) v.findViewById(R.id.record_title);
         mTitleField.setText(mRecord.getmTitle());
 
@@ -84,6 +106,18 @@ public class RecordFragment extends Fragment {
         return v;
     }
 
-
-
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        if (resultCode != Activity.RESULT_OK) {
+            return;
+        }
+        if (requestCode == REQUEST_DATE) {
+            Date date = (Date) intent.getSerializableExtra(DatePickerFragment.EXTRA_DATE);
+            mRecord.setmDate(date);
+            mDateButton.setText(mRecord.getmDate().toString());
+        }
+    }
 }
+
+
+

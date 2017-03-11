f180f2c7506cc680cd2839db85cc0ba860be99be
GDMEC07150919 <1044934446@qq.com>
2017-03-11 21:20:15 +0800

    增加功能

---
+++ app/src/main/java/com/example/com/record07150919/SQL/RecordDbSchema.java
@@ -0,0 +1,18 @@
+package com.example.com.record07150919.SQL;
+
+/**
+ * Created by 10449 on 2017/3/11.
+ */
+
+public class RecordDbSchema {
+    public static final class RecordTable {
+        public static final String NANE = "records";
+
+        public static final class Cols {
+            public static final String UUID = "uuid";
+            public static final String TITLE = "title";
+            public static final String DATE = "date";
+            public static final String SOLVED = "solved";
+        }
+    }
+}
